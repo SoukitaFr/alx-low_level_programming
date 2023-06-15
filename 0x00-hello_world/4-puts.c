67a6b704136a1bae419b39bc371864d3ff248320
SoukitaFr <soukainaferrady@gmail.com>
2023-06-15 10:47:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main - entry poit
- *return : always 0 (success)
+ *main - A programm that prints a line with puts function
+ *Return : 0 (Success)
  */
 int main(void)
 {
